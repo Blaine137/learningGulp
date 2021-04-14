6f50b15b849c9b3f40b08f183e20832665f6152d
Blaine Young <blaine.young3553@gmail.com>
2021-04-14 16:33:39 -0400

    learned gulp plugins and cross-browser css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 const { src, dest, watch } = require('gulp');
 const compileSass = require('gulp-sass');
 const minifyCss = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const postcss = require('gulp-postcss');
+const cssnext = require('postcss-cssnext');
+
+var plugins = [cssnext, autoprefixer];
 
 compileSass.compiler = require('node-sass');
 
 function bundleSass(callbackFunction) {
     src('scss/*.scss')
             .pipe(compileSass().on('error', compileSass.logError))
+            .pipe(postcss(plugins))
             .pipe(minifyCss())
             .pipe(dest('css/'));
     callbackFunction();
