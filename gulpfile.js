5857ad43dad3ea7f287f3678c42170f92ba661d0
Blaine Young <blaine.young3553@gmail.com>
2021-04-16 16:38:20 -0400

    browsersync and optimize images tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,63 @@
-const { src, dest, watch } = require('gulp');
-const compileSass = require('gulp-sass');
+const { src, dest, watch, series } = require('gulp');
+const compileSass = require('gulp-sass'); 
 const minifyCss = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const postcss = require('gulp-postcss');
+const webp = require('gulp-webp');
+const image = require('gulp-image');
 const cssnext = require('postcss-cssnext');
+const browserSync = require('browser-sync').create();
+// gulp-terser minifies javascript 
 
 var plugins = [cssnext, autoprefixer];
 
 compileSass.compiler = require('node-sass');
 
-function bundleSass(callbackFunction) {
+const sassTask = callbackFunction => {
     src('scss/*.scss')
-            .pipe(compileSass().on('error', compileSass.logError))
-            .pipe(postcss(plugins))
-            .pipe(minifyCss())
-            .pipe(dest('css/'));
-    callbackFunction();
-  }
-
-  //this task is for development mode - DONNOT FORGET TO CHANGE THE PACKAGE SCRIPT
-function devWatch(){
-    console.log('watching')
-    watch('scss/*.scss', bundleSass);
+      .pipe(compileSass().on('error', compileSass.logError))
+      .pipe(postcss(plugins))
+      /*.pipe(minifyCss()) writes all CSS on one line*/
+      .pipe(dest('css/'))
+    callbackFunction()
 }
 
-exports.default = bundleSass
-exports.devWatch = devWatch
+const imagesTask = callbackFunction => {
+  src('images/**')
+    .pipe(image())
+    .pipe(webp())
+    .pipe(dest('images/'))
+  callbackFunction()
+
+}
+
+  //creates live server
+const browsersyncServe = cb => {
+  browserSync.init({
+    server: {
+      baseDir: '.'
+    }
+  })
+  cb()
+}
+
+const browsersyncReload = cb => {
+  browserSync.reload()
+  cb()
+}
+
+
+
+  //this task is for development mode - DONOT FORGET TO CHANGE THE PACKAGE SCRIPT
+function devWatch(){
+    console.log('watching')
+    watch('*.html', browsersyncReload);
+    watch('scss/*.scss', series(sassTask, browsersyncReload));
+
+}
+
+exports.default = series(imagesTask, sassTask)
+exports.serve = series(
+  browsersyncServe,
+  devWatch
+)
